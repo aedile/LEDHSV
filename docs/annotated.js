8a611deff1505e095aa2841ca76c2d6dcb9c8483
Jesse R. Castro <aedile@me.com>
2013-05-07 22:22:28 -0500

    Docs
    Adding in documentation

---
+++ docs/annotated.js
@@ -0,0 +1,4 @@
+var annotated =
+[
+    [ "LEDHSV", "class_l_e_d_h_s_v.html", "class_l_e_d_h_s_v" ]
+];
---
+++ docs/files.js
@@ -0,0 +1,7 @@
+var files =
+[
+    [ "LEDHSV.cpp", "_l_e_d_h_s_v_8cpp.html", null ],
+    [ "LEDHSV.h", "_l_e_d_h_s_v_8h.html", [
+      [ "LEDHSV", "class_l_e_d_h_s_v.html", "class_l_e_d_h_s_v" ]
+    ] ]
+];
